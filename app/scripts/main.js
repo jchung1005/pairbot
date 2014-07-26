6072c96f102392e93dbd2271f372428f057224cf
Jeanie Chung <jeanie.chung@gmail.com>
2014-07-25 21:49:43 -0500

    Solved the task

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,75 +1,126 @@
 var string = '\n\nhpevfwqjmjryhemuqjoiatpjmddxdjwzskdcfgdtbmkbcxrnmjuoyddnqwluimjwvguxehszxzvbmufq\nlrepncxxbrrzxnzmkoyhrjcstvfazyhrhgssximjdfcmdjusylfkwbedyrsxovrmvjzaljfjmywpfnjg\nisoqbdyspgzlcmdjmhbpxhzvvhckidzuwzkauffsujmcrhvgeqvasjakgtzlxkthjqwxypmsovjbfshr\nrxtdvkmbyhejoeydnrdowuwhgmbvxmpixyttglsjgmcoqbberssfjraaqfrkmebsozsjfnubhktbbai_\nvxbifbofyednnutmxtisvfsktbqfijfzdjoqybuohtztysqelaqyixyaiolbgwylwfisfwubivuoablx\nsmrqggedwyiqvseevwbcxcfjttdbweedcjgnsorizflsjtmltcoaynsrsupavqwcyzhgiplwkohlhrai\nnazaacvuqblpbzimgoxirejbshnbmdtgsbvlhpnugggencjaczqqiwixrwiyobmlkbwdlwcioqmjhoac\ndvcqdypxeichmgywocbcafumthdqrbjnpgnnmaasxiaxxfymcyiuqduztqneodstbcnjpeebgxgosoyd\nvpzlqjuroebbehafsemanwprhwkircuhlgcftqsjdusrqetbthxclfokpdlspxzuvhxpbeqqbfpqffsg\nyilqltfxrmtimcugytazkerhcfnirtavcnmfdyictlncwttkmxyfhgejygfefqrjknuqsfldmjmwjdfq\nsicfrzbfazchdgznekwmhridelcejnkmcgmpgtihbwmplrtrrefoyhyzxpjjlkabbbgspeokzhpjxsvp\nfjmdsoripvfrgyzxodoeirwwdaofdmwqrqyvdijlfqyzfspdoyrhewxbpufdqcpqdolkmrnvedixzpfd\nakggkslxcrjbrmnynviihbkzaqqffkkcgwjbettexhlwlasdfjnslwsmnclhafvebxxfdozsjtdvobik\nrrsuysujwliobagobxmlyxjeltwzwxpyrnkdxfemotfncyriaycyfemygjmpboocgtsvttqntegvleyn\nwgpjhyyysbltoxljsascsngbgfqmpzgpejzlmdkjzzlfxvagyrasmpzqntgqsvyqjugkhbrbkiqewlyf\ntvsq_______znp_____xkwt______wef______tz______kfc_______ha_______pn__lmg__iakrbt\niyfi__uojrxvx__tps__fp__pfpndbi__ggpalde__wmd__kn__ifiadob__hdljdbd__zl__whlwilt\nbcmt__haagmjg__dwx__oh__utnzudq__xstxxyc__vly__mr__viilzav__swosyvc__i__hnaqxyev\njykc__wyfoyir__ewp__ij__mrdavxl__tcdtxqy__fnr__cf__mrkepwj__djhrsau____lhefqxgmu\nzdgf______tjg__fip__mi__b____xc__vjvhpqy______vff_____wuup_____kqct___htiggvvpet\nyvco__pqbrlox__ayj__af__dnn__kx__mlitytx____jauna__kncmiym__dlwushk____gjptzccgc\nnntt__hfqyxzi__eqn__vz__hlh__we__dtfkfvf__g__litm__zeqjtdl__bkdapxs__o__oxeouwer\nbfjr__ipcqmop__kec__ip__icc__ci__vpxxueu__eq__sau__nhheydy__efqkdgq__us__pzlndhk\nhdmk__cmfvzwcb_____xdka______trj______yj__xpi__he_______nb_______by__rrn__tvxvig\njfpseyjjbrrtsfnmbrokdqtfzhhdtbhtvpiyshmvcqaypfxcvbgvbvwrkanjfcsjnanmktkwimnvynuk\ncmgtqmovkrdmfuduqvbqydagsttictcnsrhfrpoebcehdzhjamykqpjtktufcvokljjijjsrivyhxtgw\nojgoujyhmekzsoczwlqnruwcuhudgfaijzrkewzgjvorsmabpcdmurctwjrddcnkmfvabjwlbqssihdy\nbgfqchqdvjcsdllrlwmyikuvthguzfbgocaeqktvbcapzdcfjphqnhundtljqjeyfrkjspfvghqddxwx\nidtjjkctrkfcjmdpqyvavqbntpmkkuswfgbgalrysjfnzezjjscahoodjjelavydefzjmhsqfufsexlv\nvzziymsyqrcvhsrxjnysioswvjlqdbnwgyjlanmhzkbygkptycdoifsibytbrixggjeiepaybzxhvfsy\nayeptgpxbhhfkkpromhjykfxnujorlzcmkcmvvgmveyfkgiwgosznfpmbhixsakxfkuxhwcgularehpa\nguquulrjllxmkfzgnchrxzcfdklytpfnezergkwkhgalqlvdhkdgulgfaxtybqttcjtlgmfwaymaxlwa\nspyrboibwkzzbtgigyswbtpwxgphcmkfpmvbfjimnxctinqssshofhlvlpqcwiuacjyxyqmvaibezofv\natyhpqvjubgcwqeoytloypjphoxeimumuvswxkgamodoxiciwmgxvsenkgdhttzlenjbszrksopicjcj\nnvsosrapkfilwsaoptdavlfglioqpwoqskbgikksnnuzvmxyrtrbjouvgokxgbnwxnivtykvhjkaydsk\nzoowbhjrlojgeecdoggqqtomcdgrjzmlkhubyaewwtrlyutsptdrrigopueicoganyasrjeaiivzairu\nlklovyrpckwpowprxtvhaeivpudfchxbwvtosmivpcsesbzpsynxitlisuifuehceonjeydljzuzpsgj\nllcywoxbblitscquxiykcjxhsgkbhfhfrshsrpyrcaetahuwbeybvlvkthxydkapxlfikdwudjkmjjsa\nzajxpuikiqwsifhldfovqoycwmtlmcaycirhcehxnpfadrgyaogpcmomcgtmacnvbwfnimaqqvxijcbp\nmckwimloiinindfuakqjmpyjisxnbybtywhymnkdoyiphijzelmrazplgfcmcsjiovxqdxmuqulzklgx\n\n';
 
-var stringTest = 'daccadfghd_i';
 
-var PairBot = function(string) {
-	this.string = string;
-	this.underscores = false;
+var PairBot = function PairBot() {
+	this.string = stringTest;
 	this.iteration = 0;
 	this.pairs = null;
-	this.longestPairChar = null;
+
 };
 
 PairBot.prototype = {
 	prepare: function(string) {
 		var splitString = string.split('');
-		newString = removeUnderscores(splitString);
-		return newString;
+		// newString = removeUnderscores(splitString);
+		return splitString;
+	},
+	restring: function(array) {
+		var joinedArray = array.join("");
+		return joinedArray;
 	},
 	reset: function() {
 		this.pairs = [];
 	},
-	returnPairs: function(string) {
-		var preparedString = this.prepare(string);
-		var pairs = findPairs(preparedString);
-		return pairs;
-	}
-};
+	returnPairs: function() {
+		var preparedString = this.prepare(this.string);
+		this.pairs = findPairs(preparedString);
+	},
+	process: function() {
+		do  {
+			this.iteration++;
+			this.returnPairs();
+			var preparedString = this.prepare(this.string);
+			var targetPair = checkInnerPairs(this.pairs);
+			if (targetPair === undefined) {
+				console.log("all done!");
+				this.string = this.restring(removeUnderscores(preparedString));
+				document.getElementById('output').innerHTML += "<p>Iteration: "+this.iteration+", Output: "+this.string;
+			}
+			else {
+			var shiftedArray = shiftCharacters(targetPair.start,targetPair.end,preparedString);
+					
+			this.string = this.restring(shiftedArray);
 
-function findPairs(characters) {
-	var pairs = {};
-	var pairNums = {};
-	var diffCharacters = [];
+			// append iterations to document
+			document.getElementById('output').innerHTML += "<p>Iteration: "+this.iteration+", Output: "+this.string;
+			}
+		} while (this.pairs.length != 0);
+
+		return this.string;
+	}
+}
+
+function findPairs(charArray) {
+	var pairs = [];
+	var uniqueCharacters = [];
+	var position = 0;
+
 	// create a subset of all unique characters
-	for (var i = 0; i < characters.length; i++) {
-		if (diffCharacters.indexOf(characters[i]) === -1) {
-			diffCharacters.push(characters[i])
+	for (var i = 0; i < charArray.length; i++) {
+		if (uniqueCharacters.indexOf(charArray[i]) === -1) {
+			uniqueCharacters.push(charArray[i]);
 		}
 	}
-
-	for (var i = 0; i < characters.length; i++) {
-		if (characters.indexOf(diffCharacters[i]) != -1) {
-			pairs.letter = diffCharacters[i];
-			pairs.distance = characters.indexOf(diffCharacters[i]);
-			pairs.positions = [i];
-
+	while (position < charArray.length) {
+		var currCharacter = charArray[position];
+		for (var i = 0; i < charArray.length; i++) {
+			while (currCharacter === charArray[i] && i > position) {
+				var pair = {};
+				pair.letter = currCharacter;
+				pair.start = position;
+				pair.end = i;
+				pair.distance = i - position;
+				pairs.push(pair);
+				break;
+			}
 		}
-		console.log(pairs);
+		position++;
 	}
-
-	return characters;
+	// sort by distance, largest first
+	pairs.sort(function(a,b) {
+		return b.distance - a.distance || a.start - b.start;
+	});
+	console.log(pairs);
+	return pairs;
 }
 
-function checkInnerPairs() {
+function checkInnerPairs(pairs) {
+	for (var i = 0; i < pairs.length; i++) {
+		var innerPairs = [];
+		targetStart = pairs[0].start;
+		targetEnd = pairs[0].end;
 
+		if (pairs[i].end < targetEnd && pairs[i].start > targetStart) {
+			innerPairs.push(pairs[i]);
+		}
+		if (innerPairs.length > 0) {
+			pairs.shift();
+			i = 0;
+		}
+		else {
+			target = pairs[0];
+		}
+	}
+	console.log
+	return pairs[0];
 }
 
-
-// Array Remove - By John Resig (MIT Licensed)
-Array.prototype.remove = function(from, to) {
-	var rest = this.slice((to || from) + 1 || this.length);
-	this.length = from < 0 ? this.length + from : from;
-	return this.push.apply(this, rest);
-};
+function shiftCharacters(firstChar,lastChar,array) {
+	array.splice(firstChar,1);
+	var lastChar = array.splice(lastChar-1,1);
+	array.push(lastChar[0]);
+	return array;
+}
 
 function removeUnderscores(characters) {
 	for (var i = 0; i < characters.length; i++) {
 		if (characters[i] === '_') {
-			characters.remove(i);
-		} 
+			characters.splice(i,characters.length);
+		}
 	}
 	return characters;
 }
+stringTest = 'ttvmswxjzdgzqxotby_lslonwqaipchgqdo_yz_fqdagixyrobdjtnl_jqzpptzfcdcjjcpjjnnvopmh';
 
-charactersTest = new PairBot().returnPairs(stringTest);
-document.getElementById('output').innerHTML = charactersTest;
+pairbot = new PairBot();
+pairbot.process(this.string);
